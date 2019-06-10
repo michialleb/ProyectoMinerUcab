280cfffacc29ebab80ce91805ce7dafa709f5521
michialleb <alleyne.michelle@hotmail.com>
2019-06-09 20:01:14 -0400

    m

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,7 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Hero hero="defaultHero">
         <Banner
           banner="banner"
