a9c73401ff5d15924851a14070d8f19603a28aad
Diego <diegoadq@hotmail.com>
2019-06-10 21:58:54 -0400

    .commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -18,62 +18,66 @@ import Navbar from "../components/Navbar";
 
 class Login extends Component {
   render() {
-    
     return (
-    
       <>
-      <Router basename="/login/">
-        <Switch>
-          <Route exact path="/sesion" component={Sesion} />
-          <Route path="/empleados" component={Empleado} />
-          <Route path="/yacimientos" component={Yacimientos} />
-          <Route path="/home" component={Home}></Route>
-          <div className="Login">
-            <div className="Login__Aside" />
-            <div className="Login__Form">
-              <div className="PageSwitcher">
-                <NavLink
-                  to="/sign-in"
-                  activeClassName="PageSwitcher__Item--Active"
-                  className="PageSwitcher__Item"
-                >
-                  Iniciar Sesion
-                </NavLink>
-                <NavLink
-                  exact
-                  to="/"
-                  activeClassName="PageSwitcher__Item--Active"
-                  className="PageSwitcher__Item"
-                >
-                  Registrarse
-                </NavLink>
-              </div>
+        <Router basename="/login/">
+          <Switch>
+            <Route exact path="/sesion" component={Sesion} />
+            <Route path="/empleados" component={Empleado} />
+            <Route path="/yacimientos" component={Yacimientos} />
 
-              <div className="FormTitle">
-                <NavLink
-                  to="/sign-in"
-                  activeClassName="FormTitle__Link--Active"
-                  className="FormTitle__Link"
-                >
-                  Iniciar sesion
-                </NavLink>{" "}
-                o{" "}
-                <NavLink
-                  exact
-                  to="/"
-                  activeClassName="FormTitle__Link--Active"
-                  className="FormTitle__Link"
-                >
-                  Registrarse
-                </NavLink>
+            <div className="Login">
+              <div className="Login__Aside">
+                <button className="btn-back">
+                  <Link to="/home" className="btn-back-link">
+                    Regresar
+                  </Link>
+                </button>
               </div>
+              <div className="Login__Form">
+                <div className="PageSwitcher">
+                  <NavLink
+                    to="/sign-in"
+                    activeClassName="PageSwitcher__Item--Active"
+                    className="PageSwitcher__Item"
+                  >
+                    Iniciar Sesion
+                  </NavLink>
+                  <NavLink
+                    exact
+                    to="/"
+                    activeClassName="PageSwitcher__Item--Active"
+                    className="PageSwitcher__Item"
+                  >
+                    Registrarse
+                  </NavLink>
+                </div>
 
-              <Route exact path="/" component={SignUpForm} />
-              <Route path="/sign-in" component={SignInForm} />
+                <div className="FormTitle">
+                  <NavLink
+                    to="/sign-in"
+                    activeClassName="FormTitle__Link--Active"
+                    className="FormTitle__Link"
+                  >
+                    Iniciar sesion
+                  </NavLink>{" "}
+                  o{" "}
+                  <NavLink
+                    exact
+                    to="/"
+                    activeClassName="FormTitle__Link--Active"
+                    className="FormTitle__Link"
+                  >
+                    Registrarse
+                  </NavLink>
+                </div>
+
+                <Route exact path="/" component={SignUpForm} />
+                <Route path="/sign-in" component={SignInForm} />
+              </div>
             </div>
-          </div>
-        </Switch>
-      </Router>
+          </Switch>
+        </Router>
       </>
     );
   }
