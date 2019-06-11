560c6db9c039f3580d1c9acc79d17df09fa5a032
tetecruzc <stephaniecruzcastelli@hotmail.com>
2019-06-11 15:53:11 -0400

    .

--- client/src/components/ConsultTable.js
+++ client/src/components/ConsultTable.js
@@ -3,15 +3,23 @@ import '../styles/ConsultTable.css';
 
 class ConsultTable extends Component{
     
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0};
+    
+      }
+
     render(){
     
     return(
     <>
     <table id="t01">
         <tr>
-            <th>Firstname</th>
-            <th>Lastname</th> 
-            <th>Age</th>
+        { this.props.crud.consult.map((item,i)=> 
+                 <td key={i}>
+                     {item}
+                    </td>
+        )}
         </tr>
         <tr>
             <td>Diego</td>
